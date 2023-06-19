3d64e26549f8ad6578dd137676af8cc55841c3ea
Dusko Petrovic <kalkulus@gmail.com>
2023-06-19 16:48:08 +0200

    name update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Time tracker app
+# Todo app
 
 ## Description:
 
--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Time tracker app</h1>
+        <h1>Todo app</h1>
       </div>
     </>
   );
